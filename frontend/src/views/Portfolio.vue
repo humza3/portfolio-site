9cb6911a396e94b91e4dfb1bdb57f3986c49c4a4
humza3 <humzabutt3@gmail.com>
2021-06-07 14:18:25 +0100

    added more sections to the portfolio page, node, vue, php sites

--- frontend/src/views/Portfolio.vue
+++ frontend/src/views/Portfolio.vue
@@ -1,96 +1,163 @@
 <template>
-  <div class="portfolio">
-    <h1>This is an portfolio page</h1>
-	<div id="portnav">
-		<ul>
-			<li>All Projects</li>
-			<li>VUE.js and Node.js</li>
-			<li>PHP & MYSQL</li>
-			<li>Android Apps</li>
-		</ul>
+	<div class="portfolio">
+		<h1>This is an portfolio page</h1>
+		<div id="portnav">
+			<ul>
+				<li>All Projects</li>
+				<li>VUE.js and Node.js</li>
+				<li>PHP & MYSQL</li>
+				<li>Android Apps</li>
+			</ul>
+		</div>
+		<article class="productcontent">
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.animalrun.android"><img class="img" src="../assets/hiresAR.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.animalrun.android">
+					<p class="label">Animal Run!</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.jumper.android"><img class="img" src="../assets/hiresgj.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.jumper.android">
+					<p class="label">Gnome Jumper</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.catcher.android"><img class="img" src="../assets/hiresleak.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.catcher.android">
+					<p class="label">Leak</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.invaders.android"><img class="img" src="../assets/invadershires.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.invaders.android">
+					<p class="label">The Life On Mars</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.blocko.android"><img class="img" src="../assets/Hiresmem.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.blocko.android">
+					<p class="label">MEM</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.slider.android"><img class="img" src="../assets/sliderhires.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.slider.android">
+					<p class="label">Number Slider</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.game.android"><img class="img" src="../assets/PiggieHires.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.game.android">
+					<p class="label">Piggie Bird</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawblestudios.puzlock.android"><img class="img" src="../assets/hires.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawblestudios.puzlock.android">
+					<p class="label">PuzLock</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<article class="tile">
+					<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.turtlerunner.android"><img class="img" src="../assets/Turtle-Hi-Res.png"/></a>
+				</article>
+				<a href="https://play.google.com/store/apps/details?id=com.drawablestudios.turtlerunner.android">
+					<p class="label">Tiler the Turtle</p>
+					<p class="label"> Download Now</p>
+				</a>
+			</div>
+		</article>
+		<div class="phpsites">
+			<div class ="imgcontent">
+				<div class="tile">
+					<a href="http://www.kitchen5.co.uk/"><img class="img" src="../assets/placeholder.jpg"/></a>
+				</div>
+				<a href="http://www.kitchen5.co.uk/">
+					<p class="label">Kitchen 5</p>
+					<p class="label">Visit Here</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<div class="tile">
+					<a href="http://www.mzlim.com/"><img class="img" src="../assets/placeholder.jpg"/></a>
+				</div>
+				<a href="http://www.mzlim.com/">
+					<p class="label">Mzlim</p>
+					<p class="label">Visit Here</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<div class="tile">
+					<a href="http://www.weirldnews.com/"><img class="img" src="../assets/placeholder.jpg"/></a>
+				</div>
+				<a href="http://www.weirldnews.com/">
+					<p class="label">Weirld News</p>
+					<p class="label">Visit Here</p>
+				</a>
+			</div>
+		</div>
+		<div class="vuenodesites">
+			<div class ="imgcontent">
+				<div class="tile">
+					<a href="https://github.com/humza3/interactive-portfolio"><img class="img" src="../assets/placeholder.jpg"/></a>
+				</div>
+				<a href="https://github.com/humza3/interactive-portfolio">
+					<p class="label">Interactive Portfolio</p>
+					<p class="label">Visit Repository Here</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<div class="tile">
+					<a href="https://github.com/humza3/eCommerce"><img class="img" src="../assets/placeholder.jpg"/></a>
+				</div>
+				<a href="https://github.com/humza3/eCommerce">
+					<p class="label">Frontend for Ecommerce site</p>
+					<p class="label">Visit Repository Here</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<div class="tile">
+					<a href="https://github.com/humza3/API-backend"><img class="img" src="../assets/placeholder.jpg"/></a>
+				</div>
+				<a href="https://github.com/humza3/API-backend">
+					<p class="label">Backend for API review site</p>
+					<p class="label">Visit Repository Here</p>
+				</a>
+			</div>
+			<div class ="imgcontent">
+				<div class="tile">
+					<a href="https://github.com/humza3/Groupmania"><img class="img" src="../assets/placeholder.jpg"/></a>
+				</div>
+				<a href="https://github.com/humza3/Groupmania">
+					<p class="label">Internal Social Network</p>
+					<p class="label">Visit Repository Here</p>
+				</a>
+			</div>
+		</div>
 	</div>
-	<article class="productcontent">
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.animalrun.android"><img class="img" src="../assets/hiresAR.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.animalrun.android">
-                    <p class="label">Animal Run!</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.jumper.android"><img class="img" src="../assets/hiresgj.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.jumper.android">
-                    <p class="label">Gnome Jumper</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.catcher.android"><img class="img" src="../assets/hiresleak.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.catcher.android">
-                    <p class="label">Leak</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.invaders.android"><img class="img" src="../assets/invadershires.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.invaders.android">
-                    <p class="label">The Life On Mars</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.blocko.android"><img class="img" src="../assets/Hiresmem.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.blocko.android">
-                    <p class="label">MEM</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.slider.android"><img class="img" src="../assets/sliderhires.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.slider.android">
-                    <p class="label">Number Slider</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.game.android"><img class="img" src="../assets/PiggieHires.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.game.android">
-                    <p class="label">Piggie Bird</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawblestudios.puzlock.android"><img class="img" src="../assets/hires.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawblestudios.puzlock.android">
-                    <p class="label">PuzLock</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-            <div class ="imgcontent">
-                <article class="tile">
-                    <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.turtlerunner.android"><img class="img" src="../assets/Turtle-Hi-Res.png"/></a>
-                </article>
-                <a href="https://play.google.com/store/apps/details?id=com.drawablestudios.turtlerunner.android">
-                    <p class="label">Tiler the Turtle</p>
-                    <p class="label"> Download Now</p>
-                </a>
-            </div>
-        </article>
-  </div>
 </template>
